47b5475f3a66f1f960d43a7f400ed5e336c32e3a
Ali Hyder <cw.alihyder@gmail.com>
2022-05-28 16:13:36 +0500

    laravel-error-response-snippest

---
+++ javascript/laravel/errorSnippest.js
@@ -0,0 +1,15 @@
+/*
+* Laralvel Ajax Request response errors appending
+* just place your element with id formErrors and place the code within the errorsList in error: function(errors) => { erorrList(errors)  }
+*/
+ function errorsList(errors) {
+    var errorsList = '<ul>'
+    $.each(errors.responseJSON.errors, function (indexInArray, valueOfElement) { 
+        errorsList += '<li>'+valueOfElement[0]+'</li>'
+    });
+    errorsList +='<ul>'
+
+    $('#formErrors').append(errorsList);
+    $('#formErrors').show();
+
+}
